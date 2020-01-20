4a6bda1eb52526ac38f31bc7c3f7ded76377afed
raniaw <raniaw@web.de>
2020-01-20 23:13:26 +0100

    Updates

---
+++ precache-manifest.98bf4f19507eb1ad73f8cfe35d4f218f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657f1dc64b3027112bd1f468bb98de56",
+    "url": "/app-pixabay-search/index.html"
+  },
+  {
+    "revision": "3a331a667e8f36349521",
+    "url": "/app-pixabay-search/static/css/main.e188b651.chunk.css"
+  },
+  {
+    "revision": "344de31d4746367c9bbe",
+    "url": "/app-pixabay-search/static/js/2.b4cd80c3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app-pixabay-search/static/js/2.b4cd80c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a331a667e8f36349521",
+    "url": "/app-pixabay-search/static/js/main.884261e7.chunk.js"
+  },
+  {
+    "revision": "4cc048bf022a74f73d7f",
+    "url": "/app-pixabay-search/static/js/runtime-main.1882caad.js"
+  },
+  {
+    "revision": "a57a395bf0a4395b3a7d961f88606c52",
+    "url": "/app-pixabay-search/static/media/DoubleRing.a57a395b.gif"
+  }
+]);
